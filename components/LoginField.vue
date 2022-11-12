18f182bee3d176cb842fef4bc4c9aae67821e3e1
João Vitor Denari dos Santos <98748740+jdenari@users.noreply.github.com>
2022-11-11 23:36:17 -0300

    input-component-added

---
+++ components/LoginField.vue
@@ -0,0 +1,67 @@
+<template>
+    <div>
+       <form class="w-25 p-3">
+           <div class="mb-3">
+               <label for="exampleInputEmail1" class="form-label">Email address</label>
+               <input 
+                   type="email" 
+                   class="form-control" 
+                   id="exampleInputEmail1" 
+                   aria-describedby="emailHelp"
+                   name="login"
+                   v-model="auth.email"
+               >
+           </div>
+           <div class="mb-3">
+               <label for="exampleInputPassword1" class="form-label">Password</label>
+               <input 
+                   type="password" 
+                   class="form-control" 
+                   id="exampleInputPassword1"
+                   name="password"
+                   v-model="auth.password"
+               >
+           </div>
+           <button 
+               type="button" 
+               class="btn btn-primary w-100"
+               @click="login"
+               >Submit
+           </button>
+           <p>
+               {{ errorText }}
+           </p>
+           <NuxtLink to="/">GO TO LANDING PAGE</NuxtLink>
+       </form>
+   </div>
+</template>
+
+<script>
+export default {
+    name: 'LoginField',
+    data() {
+       return {
+           errorText: 'No error message',
+           auth: {
+               email: '',
+               password: ''
+            }
+        }
+    },
+    methods: {
+       login() {
+           this.$fire.auth.signInWithEmailAndPassword(this.auth.email, this.auth.password)
+           .catch((error) => {
+               this.errorText = error
+           }).then((user) => {
+               //we are signed in
+               redirect ('/client/home')
+           })
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
--- pages/auth/signin.vue
+++ pages/auth/signin.vue
@@ -1,64 +1,14 @@
 <template>
      <div>
-        <form class="w-25 p-3">
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input 
-                    type="email" 
-                    class="form-control" 
-                    id="exampleInputEmail1" 
-                    aria-describedby="emailHelp"
-                    name="login"
-                    v-model="auth.email"
-                >
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input 
-                    type="password" 
-                    class="form-control" 
-                    id="exampleInputPassword1"
-                    name="password"
-                    v-model="auth.password"
-                >
-            </div>
-            <button 
-                type="button" 
-                class="btn btn-primary w-100"
-                @click="login"
-                >Submit
-            </button>
-            <p>
-                {{ errorText }}
-            </p>
-            <NuxtLink to="/">GO TO LANDING PAGE</NuxtLink>
-        </form>
+        <LoginField />
     </div>
 </template>
 
 <script>
+
 export default {
-    data() {
-        return {
-            errorText: 'No error message',
-            auth: {
-                email: '',
-                password: ''
-            }
-        }
-    },
-    methods: {
-        login() {
-            this.$fire.auth.signInWithEmailAndPassword(this.auth.email, this.auth.password)
-            .catch(function (error){
-                
-                this.errorText = error
-            }).then((user) => {
-                //we are signed in
-                $nuxt.$router.push('/client/home')
-            })
-        },
-    }
+    data() {},
+    methods: {}
 }
 </script>
 
