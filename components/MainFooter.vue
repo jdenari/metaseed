8c8d74c999adfbb9fb8efd1604d783ab5c21cf6e
João Vitor Denari dos Santos <98748740+jdenari@users.noreply.github.com>
2022-11-13 21:43:02 -0300

    footercreated

---
+++ components/MainFooter.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container border-top p-3 d-flex align-items-center">
+        <div class="container d-flex px-4 align-items-center">
+            <img src="../static/logo/logo-metaseed-small.png" alt="" class="p-1">
+            <div class="h5">
+                Metaseed Company
+            </div>
+        </div>      
+        <a  href="https://www.instagram.com/metaseed.co/" 
+            class="" 
+            target="_blank" 
+        >
+            <img src="../static/icons/instagram.png" 
+            alt="" 
+            class="p-2" 
+            >
+        </a>
+        <a  href="https://www.instagram.com/metaseed.co/" 
+            class="" 
+            target="_blank" 
+        >
+            <img src="../static/icons/instagram.png"
+            alt="" 
+            class="p-2" 
+            >
+        </a>
+        <a  href="https://www.instagram.com/metaseed.co/" 
+            class="" 
+            target="_blank" 
+        >
+            <img src="../static/icons/instagram.png"
+            alt="" 
+            class="p-2" 
+            >
+        </a>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'MainFooter',
+    }
+</script>
--- pages/auth/signin.vue
+++ pages/auth/signin.vue
@@ -2,16 +2,19 @@
      <div class="p-3">
         <MainHeader />
         <LoginField />
+        <MainFooter />
     </div>
 </template>
 
 <script>
 import MainHeader from '../../components/MainHeader.vue';
 import LoginField from '../../components/SigninPage/LoginField.vue';
+import MainFooter from '../../components/MainFooter.vue';
 export default {
     components: {
         MainHeader
         , LoginField
+        , MainFooter
     },
     methods: {}
 }
