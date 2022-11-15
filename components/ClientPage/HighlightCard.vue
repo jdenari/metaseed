bdff1ebf53e68a9676f898eeb896ee901d2d257f
João Vitor Denari dos Santos <98748740+jdenari@users.noreply.github.com>
2022-11-15 15:08:33 -0300

    top-dashboard-done

---
+++ components/ClientPage/HighlightCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="card-variable bg-light rounded p-2 px-5">
+        <h5> {{ cardNumber }}</h5>
+        <h3>{{ cardVariable }}</h3>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'HighlightCard',
+    props: ['cardVariable', 'cardNumber']
+}
+</script>
+
+<style scoped>
+
+    .card-variable{
+        width: 260px;
+    }
+
+</style>
--- pages/client/home.vue
+++ pages/client/home.vue
@@ -1,20 +1,24 @@
 <template>
     <div>
-      <MainHeader />
-      <div class="d-flex">
-        <SideBar />
-      </div>
-      
+        <MainHeader />
+        <div class="d-flex">
+            <SideBar />
+            <div class="w-100">
+                <TopDashboard class="top-dashboard d-flex justify-content-center p-3" />
+            </div>
+        </div>
     </div>
   </template>
 
 <script>
 import SideBar from '../../components/ClientPage/SideBar.vue';
 import MainHeader from '../../components/MainHeader.vue'
+import TopDashboard from '../../components/ClientPage/TopDashboard.vue'
     export default {
         components: {
             SideBar
             , MainHeader
+            , TopDashboard
         },
         // middleware: ['auth']
     }
